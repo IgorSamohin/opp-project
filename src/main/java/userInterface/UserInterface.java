de602fd35b956f0835cf787ce78da8af23367fd0
IgorSamohin <igor04012001@yandex.ru>
2021-03-04 21:20:33 +0300

    Implement user interface (service 2)

--- src/main/java/userInterface/UserInterface.java
+++ src/main/java/userInterface/UserInterface.java
@@ -1,8 +1,19 @@
 package userInterface;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import generator.ScheduleGenerator;
+import generator.cargo.Cargo;
+import generator.cargo.CargoType;
 import generator.ship.Ship;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
 
 /**
  * Service-2.
@@ -17,22 +28,79 @@ public class UserInterface {
     public UserInterface(int loaderPerformance, int amountOfLoaders) {
         this.loaderPerformance = loaderPerformance;
         this.amountOfLoaders = amountOfLoaders;
-        this.scheduleGenerator = new ScheduleGenerator(loaderPerformance,amountOfLoaders);
+        this.scheduleGenerator = new ScheduleGenerator(loaderPerformance, amountOfLoaders);
     }
 
     /**
      * In part 2 this method will do GET-request
      */
-    private List<Ship> getSchedule(){
+    private List<Ship> getSchedule() {
         return scheduleGenerator.getSchedule();
     }
 
-    private void writeJson(List<Ship> ships, String fileName){
+    /**
+     * Transform schedule to json objects and writes it to a file
+     */
+    private void writeScheduleInJsonFile(List<Ship> ships, String fileName) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        String jsonSchedule = mapper.writeValueAsString(ships);
+        File file = new File(fileName);
 
+        try (FileWriter writer = new FileWriter(file)) {
+            writer.write(jsonSchedule);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 
-    private Ship readShip(){
-        return (Ship) new Object();
+    /**
+     * @return a ship read from terminal or error
+     */
+    private Ship readShip() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print("Name (String): ");
+        String name = reader.readLine();
+
+        System.out.print("Cargo params (int): ");
+        int params = Integer.parseInt(reader.readLine());
+
+        System.out.print("Cargo type (BULK, LIQUID, CONTAINER): ");
+        CargoType type = switch (reader.readLine().toUpperCase(Locale.ROOT)) {
+            case "BULK" -> CargoType.BULK;
+            case "LIQUID" -> CargoType.LIQUID;
+            case "CONTAINER" -> CargoType.CONTAINER;
+            default -> throw new IOException();
+        };
+
+        System.out.print("Arrival date (int): ");
+        int date = Integer.parseInt(reader.readLine());
+
+        System.out.print("Unloading time (int): ");
+        int time = Integer.parseInt(reader.readLine());
+
+        return new Ship(name, new Cargo(type, params), date, time);
     }
 
+    public void run() throws JsonProcessingException {
+        Scanner in = new Scanner(System.in);
+        List<Ship> schedule = this.getSchedule();
+
+        System.out.println("Schedule was generated.");
+        while (true) {
+            System.out.println("Do you want to enter data? (N|y)");
+            String command = in.nextLine();
+            if (command.equals("y") || command.equals("Y")) {
+                System.out.println("Enter data in format: ");
+                try {
+                    schedule.add(readShip());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                break;
+            }
+        }
+
+        writeScheduleInJsonFile(schedule, "src/main/resources/json.json");
+    }
 }
