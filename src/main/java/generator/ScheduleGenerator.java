ebc2c87bc44cb27fd06c038a63845560a217fff7
IgorSamohin <igor04012001@yandex.ru>
2021-03-07 02:07:40 +0300

    Delete unused field in schedule generator, refactor code

--- src/main/java/generator/ScheduleGenerator.java
+++ src/main/java/generator/ScheduleGenerator.java
@@ -11,12 +11,10 @@ import java.util.List;
  */
 public class ScheduleGenerator {
     private final int loaderPerformance;
-    private final int amountOfLoaders; //todo сделать учет этого поля в расчетах
     private final static int MAX_MINUTES = 43_200; //todo потенциально нужно вести 3 разных времени, отвечающих за разные потоки разгрузки, чтобы не было простоев у кранов
 
-    public ScheduleGenerator(int loaderPerformance, int amountOfLoaders) {
+    public ScheduleGenerator(int loaderPerformance) {
         this.loaderPerformance = loaderPerformance;
-        this.amountOfLoaders = amountOfLoaders;
     }
 
     public List<Ship> getSchedule() {
@@ -32,20 +30,18 @@ public class ScheduleGenerator {
     private void writeSchedule(List<Ship> ships) {//todo сделать из этого нормальную табличку с нормальным формированием времени
         System.out.println("Current schedule: ");
         for (Ship ship : ships) {
-            double unloadingTime = ship.getUnloadingTime();
-            int days1 = (int) (unloadingTime / 1440.0);
-            int hours1 = (int) ((unloadingTime - days1 * 1440.0) / 60.0);
-            int minutes1 = (int) (unloadingTime - hours1 * 60 - days1 * 1440);
-            String unloadingTime1 = "" + days1 + ":" + hours1 + ":" + minutes1;
-
-            double unloadingTime2 = ship.getArrivalDate();
-            int days2 = (int) (unloadingTime2 / 1440.0);
-            int hours2 = (int) ((unloadingTime2 - days2 * 1440.0) / 60.0);
-            int minutes2 = (int) (unloadingTime2 - hours2 * 60 - days2 * 1440);
-            String unloadingTime3 = "" + days2 + ":" + hours2 + ":" + minutes2;
+            String unloadingTime = formatDate(ship.getUnloadingTime());
+            String arrivalDate = formatDate(ship.getArrivalDate());
 
             System.out.printf("Name: %s, Cargo type: %-9s, Cargo parameters: %-6s, Arrival date: %-8s, Planned unloading time: %-8s \n",
-                    ship.getName(), ship.getCargo().getCargoType(), ship.getCargo().getParams(), unloadingTime3, unloadingTime1);
+                    ship.getName(), ship.getCargo().getCargoType(), ship.getCargo().getParams(), arrivalDate, unloadingTime);
         }
+    }
+
+    private String formatDate(double time) {
+        int days = (int) (time / 1440.0);
+        int hours = (int) ((time - days * 1440.0) / 60.0);
+        int minutes = (int) (time - hours * 60 - days * 1440);
+        return  "" + days + ":" + hours + ":" + minutes;
     }
 }
