579b91c10fb58ac7476b6c4ea2c3bb89701f33e1
IgorSamohin <igor04012001@yandex.ru>
2021-03-07 02:54:48 +0300

    Refactor simulator

--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.databind.type.CollectionType;
 import generator.ship.Ship;
 import java.io.File;
 import java.io.IOException;
-import static java.lang.Math.min;
+import java.lang.Math;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -21,7 +21,7 @@ import java.util.concurrent.Phaser;
 public class Simulator {
     private int amountOfLoaders = 5;
     private int loaderPerformance = 100;
-    Random random = new Random();
+    private Random random = new Random();
     private final int maxDelay = 10_080;
     private final int maxTime = 43_200;
     private List<Thread> loaders = new ArrayList<>();
@@ -29,7 +29,6 @@ public class Simulator {
     private List<Ship> report = new ArrayList<>();
     private ConcurrentLinkedQueue<Ship> arrivedShips = new ConcurrentLinkedQueue<>();
     private int currentTime = 0;
-    private boolean canWork = false;
 
     public List<Ship> getReport() {
         return report;
@@ -38,13 +37,16 @@ public class Simulator {
     private void makeDelays(List<Ship> ships) {
         for (Ship ship : ships) {
             int delay = random.nextInt(maxDelay * 2) - maxDelay;
-            if(delay < 0){
-                delay = -min(ship.getArrivalDate(), -delay);
+            if (delay < 0) {
+                delay = -Math.min(ship.getArrivalDate(), -delay);
             }
             ship.increaseArrivalDate(delay);
         }
     }
 
+    /**
+     * Do preparations before simulation beginning
+     */
     public void run() throws IOException, BrokenBarrierException, InterruptedException {
         List<Ship> schedule = this.getSchedule();
         List<Ship> actualSchedule = new ArrayList<>(schedule);
@@ -53,49 +55,84 @@ public class Simulator {
 
         actualSchedule.sort(Comparator.comparingInt(Ship::getArrivalDate));
 
-        this.startSimulation(schedule, actualSchedule);
-    }
-
-    private void startSimulation(List<Ship> schedule, List<Ship> actualSchedule) {
         Phaser phaser = new Phaser(amountOfLoaders + 1);
 
+        this.startThreads(phaser);
+
+        this.startMainCycle(actualSchedule, phaser);
+
+        this.interruptThreads();
+    }
+
+    /**
+     * Auxiliary method
+     *
+     * @return id of worker which will be taken by current thread
+     */
+    private int takeWorker() {
+        for (int j = 0; j < amountOfLoaders; j++) {
+            if (workers.get(j).takePlace()) {
+                return j;
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * Creates threads with tasks and fill worker's array
+     *
+     * @param phaser needs for synchronization between loaders and main thread
+     */
+    private void startThreads(Phaser phaser) {
         for (int i = 0; i < amountOfLoaders; i++) {
             workers.add(new Worker(arrivedShips, loaderPerformance, currentTime));
 
-            loaders.add(new Thread(() -> {
-                int takenWorker = -1;
+            Runnable r = () -> threadCycle(phaser);
+            loaders.add(new Thread(r));
 
-                while (true) {
-
-                    phaser.arriveAndAwaitAdvance();
-
-                    if ((takenWorker >= 0) && (workers.get(takenWorker).isUnloaded())) {
-                        workers.get(takenWorker).release();
-                        takenWorker = -1;
-                    }
-
-                    if (takenWorker < 0) {
-                        for (int j = 0; j < amountOfLoaders; j++) {
-                            if (workers.get(j).takePlace()) {
-                                takenWorker = j;
-                                break;
-                            }
-                        }
-                    }
-
-                    if (takenWorker >= 0) {
-                        workers.get(takenWorker).work();
-                    }
-
-                    phaser.arriveAndAwaitAdvance();
-                }
-            }));
-        }
-
-        for (int i = 0; i < amountOfLoaders; i++) {
             loaders.get(i).start();
         }
+    }
 
+    /**
+     * Declare task for loader
+     *
+     * @param phaser needs for synchronization between loaders and main thread
+     */
+    private void threadCycle(Phaser phaser) {
+        int takenWorker = -1;
+
+        while (true) {
+            phaser.arriveAndAwaitAdvance();
+
+            //check if taken worker empty or unloaded
+            if ((takenWorker >= 0) && (workers.get(takenWorker).isUnloaded() || !workers.get(takenWorker).isBusy())) {
+                workers.get(takenWorker).release();
+                takenWorker = -1;
+            }
+
+            //try to take new worker if thread is not "busy"
+            if (takenWorker < 0) {
+                takenWorker = takeWorker();
+            }
+
+            //do work if thread have taken a worker
+            if (takenWorker >= 0) {
+                workers.get(takenWorker).work();
+            }
+
+            phaser.arriveAndAwaitAdvance();
+        }
+    }
+
+
+    /**
+     * Main cycle. Manage synchronization, update workers
+     *
+     * @param actualSchedule - schedule with delays
+     * @param phaser         needs for synchronization between loaders and main thread
+     */
+    private void startMainCycle(List<Ship> actualSchedule, Phaser phaser) {
         while (currentTime != maxTime) {
             while (!(actualSchedule.isEmpty()) && (actualSchedule.get(0).getArrivalDate() == currentTime)) {
                 arrivedShips.add(actualSchedule.remove(0));
@@ -108,17 +145,17 @@ public class Simulator {
                 }
             }
 
-            canWork = true;
             phaser.arriveAndAwaitAdvance();
 
             ++currentTime;
             phaser.arriveAndAwaitAdvance();
         }
+    }
 
+    private void interruptThreads() {
         for (int i = 0; i < amountOfLoaders; i++) {
             loaders.get(i).interrupt();//todo потоки почему-то не гасятся
         }
-
     }
 
     /**
